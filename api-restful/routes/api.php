8872cd2718179b9c302dfc1f1c76bec5ea1ccb75
w2038 <w4gn3r2038@gmail.com>
2023-05-14 23:48:26 -0300

    novas rotas

--- api-restful/routes/api.php
+++ api-restful/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\CategoriaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +20,12 @@ use App\Http\Controllers\ProdutoController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/categorias', [CategoriaController::class, 'index']);
+Route::get('/categorias/{id}', [CategoriaController::class, 'show']);
+Route::post('/categorias', [CategoriaController::class, 'store']);
+Route::put('/categorias/{id}', [CategoriaController::class, 'update']);
+Route::delete('/categorias/{id}', [CategoriaController::class, 'destroy']);
 
 Route::get('produtos', [ProdutoController::class, 'index']);
 Route::get('produtos/{id}', [ProdutoController::class, 'show']);
